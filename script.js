fcd96e7e759ca7231cbf493f5ed80db78e1a5157
Shakirullah_304 <shakir.edu304@gmail.com>
2024-10-12 05:48:33 -0700

    create pet object with properties

---
+++ script.js
@@ -0,0 +1,8 @@
+let pet = {
+    name: prompt("Enter Your Pet Name: "),
+    type: prompt("What kind of pet is it?"),
+    age: +prompt("How old is your pet?"),
+    happeness: +prompt("How happy is your pet? (a number between 0 and 100)."),
+    hunger: +prompt("How hungry is your pet? (a number between 0 and 100)."),
+
+}
